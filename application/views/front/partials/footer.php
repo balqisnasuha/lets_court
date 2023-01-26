0b6221c8565139f7d58b2708e9c311bfe54732dd
balqisnasuha <qixsmus@gmail.com>
2023-01-26 18:02:16 +0800

    test

--- application/views/front/partials/footer.php
+++ application/views/front/partials/footer.php
@@ -1,5 +1,5 @@
 <footer class="section p-3 bg-dark text-white">
-    <div class="text-center">&copy; Copyright <?php echo date("Y"); ?> Food Ordering System</div>
+    <div class="text-center">&copy; Copyright <?php echo date("Y"); ?> Let's Court Booking System</div>
 </footer>
 </body>
 
